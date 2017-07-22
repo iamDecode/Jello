2d6b2bdadad048f39f998f37f60670a62843fb11
Dennis Collaris <d.collaris@me.com>
2017-07-22 23:29:41 +0200

    Initial Commit

---
+++ Jello/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Jello
+//
+//  Created by Dennis Collaris on 22/07/2017.
+//  Copyright © 2017 collaris. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
---
+++ JelloTests/JelloTests.swift
@@ -0,0 +1,36 @@
+//
+//  JelloTests.swift
+//  JelloTests
+//
+//  Created by Dennis Collaris on 22/07/2017.
+//  Copyright © 2017 collaris. All rights reserved.
+//
+
+import XCTest
+@testable import Jello
+
+class JelloTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
