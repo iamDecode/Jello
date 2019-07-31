967e0993e84c0cffa076c76ca34e061529be6d03
Dennis Collaris <d.collaris@me.com>
2019-07-31 14:57:14 +0200

    clean up project and remove spritekit gamescene

--- Jello/ViewController.swift
+++ Jello/ViewController.swift
@@ -7,31 +7,11 @@
 //
 
 import Cocoa
-import SpriteKit
-import GameplayKit
 
 class ViewController: NSViewController {
 
-    @IBOutlet var skView: SKView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let view = self.skView {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFit
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
     }
 }
 
